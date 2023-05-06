d0ff385bd80f160b96fa9b49ebc7643d32c9ff61
Alisa <alisakovalhome@mail.ru>
2023-05-06 11:27:22 +0300

    add defolt in part1

--- part-1/doughnut.js
+++ part-1/doughnut.js
@@ -1,6 +1,6 @@
 const Stuffing = require('./stuffing')
 module.exports = class Doughnut extends Stuffing {
-  constructor({ name: name }, stuffings) {
+  constructor({ name = 'Пышка' }, stuffings) {
     super(name)
     this.stuffings = stuffings
 
